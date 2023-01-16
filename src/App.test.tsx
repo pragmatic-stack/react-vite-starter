b1594771c9fc3d698b5cc02a69fa8007a0965c87
Konstantin Kraska <office@kraska-systems.de>
2023-01-16 22:04:07 +0100

    refactor: cleanup App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -9,12 +9,6 @@ describe('App', () => {
 
     expect(screen.getByText('App')).toBeInTheDocument();
   });
-
-  it('should fetch todos', async () => {
-    const todos = await fetch('https://localhost:3000/todos').then((data) => data.json());
-
-    expect(todos.data).toHaveLength(0);
-  });
 });
 
 export {};
